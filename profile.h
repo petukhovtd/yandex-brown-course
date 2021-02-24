15c7555d34663dd9e0f88a36b0d52ec883a9d173
Petukhov Timofey <Timofey.Petukhov@infotecs.ru>
2021-02-24 21:16:45 +0700

    concurrent map 2

--- profile.h
+++ profile.h
@@ -17,7 +17,7 @@ public:
           auto finish = std::chrono::steady_clock::now();
           auto dur = finish - start;
           std::cerr << message
-               << duration_cast<std::chrono::milliseconds>(dur).count()
+               << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count()
                << " ms" << std::endl;
      }
 private:
